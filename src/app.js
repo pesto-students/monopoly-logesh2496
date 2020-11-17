76f0e2fee1b1c87bf05c7d0a3d84f915f6303634
Logesh <logesh.r24@gmail.com>
2020-11-17 17:28:00 +0530

    add chances, cc and no of playeres

--- src/app.js
+++ src/app.js
@@ -35,8 +35,16 @@ const endTurnBtn = getById('end_turn');
 function startGame() {
     const playerOneSettings = getPlayersInfo(getById('player_one_settings'));
     const playerTwoSettings = getPlayersInfo(getById('player_two_settings'));
+    const playerThreeSettings = getPlayersInfo(getById('player_three_settings'));
+    const playerFourSettings = getPlayersInfo(getById('player_four_settings'));
+    const players = [playerOneSettings, playerTwoSettings, playerThreeSettings, playerFourSettings];
+    const noOfPlayers = parseInt(getById('no_of_players').value);
+    for(let i=1;i<=noOfPlayers;i++){
+        const id = `dyn_name_${i}`;
+        getById(id).innerHTML = players[i-1].name;
+    }
     hideUnwantedElements(true);
-    monopolyGame[instanceAttribute] = new GameSettings([playerOneSettings, playerTwoSettings]);
+    monopolyGame[instanceAttribute] = new GameSettings(players, noOfPlayers);
 }
 function hideUnwantedElements(isGamePage) {
     if (isGamePage) {
@@ -51,6 +59,7 @@ function rollDice() {
     getById('dice_one').children[diceOne - 1].style.display = 'unset';
     getById('dice_two').children[diceTwo - 1].style.display = 'unset';
     const totalIncrement = diceOne + diceTwo;
+    monoPoly.currentDice = totalIncrement;
     const player = monoPoly.playersInfo[monoPoly.playerTurn];
     player.setPosition(totalIncrement);
     rollDiceBtn.setAttribute('disabled', true);
@@ -88,11 +97,11 @@ function performPlayerAction(monoPoly, player, lastDiceValues) {
                 monoPoly.owned.push({ position: currentPosition, player: monoPoly.playerTurn });
                 player.buy(card);
             }
-            endTurn();
+            // endTurn();
         } else {
             switch (card.name) {
                 case 'Community Chest':
-                    alert(communityCards[lastDiceValues - 1]);
+                    handleCommunityCards(lastDiceValues - 1, player);
                     break;
                 case 'City Tax':
                     player.cash -= 200;
@@ -109,6 +118,10 @@ function performPlayerAction(monoPoly, player, lastDiceValues) {
                     break;
                 case 'Go to Jail':
                     break;
+                case 'LUXURY TAX':
+                    player.cash -= 200;
+                    alert('Luxury tax amount $100 has been deducted!');
+                    break;
                 default:
                     alert('Something went wrong, please report it to a dev!');
             }
@@ -116,10 +129,112 @@ function performPlayerAction(monoPoly, player, lastDiceValues) {
     }
 
 }
+function handleCommunityCards(diceValue, player){
+    const ccStr = communityCards[diceValue];
+    alert(ccStr);
+    switch(diceValue){
+        case 1:
+            player.cash += 10;
+            break;
+        case 2:
+            player.cash += 50;
+            break;
+        case 3:
+        case 5:
+        case 6:
+            player.cash += 100;
+            break;
+        case 4:
+            player.cash += 20;
+            break;
+        case 7:
+            player.cash += 25;
+            break;
+        case 8:
+            player.cash -= 100;
+            break;
+        case 9:
+            player.cash += 200;
+            break;
+        case 10:
+        case 11:
+            player.cash -= 50;
+            break;
+        case 12:
+            player.cash += 20;
+            break;
+        case 13:
+            player.setPosition(false, 0);
+            break;
+        case 14:
+            //TODO
+            break;
+        case 15:
+            player.setPosition(false, 10);
+            break;
+    }
+}
+function handleChances(diceValue, player){
+    const ccStr = chances[diceValue];
+    alert(ccStr);
+    switch(diceValue){
+        case 1:
+            //TODO
+            break;
+        case 2:
+            player.cash -= 15;
+            break;
+        case 3:
+            //TODO
+            player.cash -= 50;
+            break;
+        case 4:
+            const currentPosition = player.getPosition();
+            player.setPosition(false, currentPosition - 3);
+            break;
+        case 5:
+            //TODO
+            player.cash += 50;
+            break;
+        case 6:
+            player.cash += 50;
+            break;
+        case 7:
+            //TODO
+            break;
+        case 8:
+            player.cash -= 15;
+            break;
+        case 9:
+            player.setPosition(false, 5);
+            player.cash += 200;
+            break;
+        case 10:
+            player.setPosition(false, 39);
+            break;
+        case 11:
+            player.setPosition(false, 24);
+            //TODO
+            break;
+        case 12:
+            player.cash += 150;
+            break;
+        case 13:
+            //TODO
+            break;
+        case 14:
+            player.setPosition(false, 11);
+            //TODO
+            break;
+        case 15:
+            player.setPosition(false, 10);
+            break;
+    }
+}
 //Classes
 class GameSettings {
-    constructor(playersInfo) {
-        this.playersInfo = playersInfo.map(({ name, color }, i) => {
+    constructor(playersInfo, noOfPlayers) {
+        this.playersInfo = playersInfo.slice(0, noOfPlayers).map(({ name, color }, i) => {
             const playerProps = {
                 name,
                 color,
@@ -129,7 +244,7 @@ class GameSettings {
             };
             return new Player(playerProps);
         });
-        this.noOfPlayers = playersInfo.length;
+        this.noOfPlayers = noOfPlayers;
         this.playerTurn = 0;
         this.owned = [];
     }
@@ -142,8 +257,13 @@ class GameSettings {
     }
     transferRent(property, from, to) {
         const payer = this.playersInfo[from];
+        const owener = this.playersInfo[to];
         const rentIndex = payer.rentpayHistory[property.name] || 1;
-        const rentAmount = property[`rent${rentIndex}`];
+        let rentAmount = property[`rent${rentIndex}`];
+        if(property.groupNumber === 2){
+            const diceValue = getInstance().currentDice;
+            rentAmount = owener.properties.filter(property => property.groupNumber === 2).length === 2 ? diceValue * 10 : diceValue * 2; 
+        }
         alert(`Rent for visiting the place ${property.name} amount: $${rentAmount} will be detucted`);
         payer.addRentPayHistory(property.name);
         if (property[`rent${rentIndex}`] <= payer.cash) {
@@ -151,6 +271,8 @@ class GameSettings {
         } else {
             alert('please sell a property to pay the debt!!!');
         }
+        const table = getById(`player_${payer.id}_table`);
+        table.rows[0].cells[0].querySelector('#cash_on_hand').innerHTML = `$${payer.cash}`;
     }
 }
 class Player {
@@ -164,17 +286,30 @@ class Player {
         this.properties = [];
         this.rentpayHistory = {};
         this.freeFromJailPass = 0;
+        this.collectForGo = false;
     }
     getPosition() {
         return this.#_position;
     }
-    setPosition(diceValue) {
-        this.#_prevPosition = this.#_position;
-        const newPosition = this.#_position + diceValue;
-        if (newPosition > 39) {
-            this.#_position = newPosition - 39;
+    setPosition(diceValue, toValue) {
+        if(toValue){
+            this.#_position = 0;
         } else {
-            this.#_position = newPosition;
+            this.#_prevPosition = this.#_position;
+            const newPosition = this.#_position + diceValue;
+            if (newPosition > 39) {
+                this.#_position = newPosition - 39;
+            } else {
+                this.#_position = newPosition;
+            }
+        }
+        if(this.collectForGo && this.#_position > 0 && this.#_position <= 12){
+            this.collectForGo = false;
+            this.cash += 200;
+            alert('$200 has been credited for crossing GO!');
+        }
+        if(this.#_position > 27 && this.#_position <= 39){
+            this.collectForGo = true;
         }
         this.setElementToPosition();
     }
@@ -188,6 +323,10 @@ class Player {
         position.children[this.id].style.visibility = 'hidden';
     }
     buy(card) {
+        if(this.cash < card.price){
+            alert(`You don't have sufficient money to buy this property.`);
+            return;
+        }
         this.cash -= card.price;
         this.properties.push(card);
         const table = getById(`player_${this.id}_table`);
