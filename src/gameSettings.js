bb20445315818d0c4875654403413c3600ee264d
Logesh <logesh.r24@gmail.com>
2020-11-27 08:22:59 +0530

    fix multiple script reference

--- src/gameSettings.js
+++ src/gameSettings.js
@@ -70,7 +70,7 @@ export default class GameSettings {
                 this.disableRollDiceBtn();
                 this.enableEndTurnBtn();
             }
-        }, 10);
+        }, 100);
     }
     endTurn() {
         this.hideDices();
